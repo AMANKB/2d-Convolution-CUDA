29b35ee41a585372516fa2920f566bfa142f2b39
YangChen <yangchen24110@gmail.com>
2020-06-11 10:04:20 +0800

    Update convolution.cu

--- convolution.cu
+++ convolution.cu
@@ -14,7 +14,7 @@
 #define HB (HA - HC + 1)
 
 
-__global__ void Convolution(float* A, float* B, float* C, int numARows, int numACols, int numBRows, int numBCols, int numCRows, int numCCols)
+__global__ void Convolution2(float* A, float* B, float* C, int numARows, int numACols, int numBRows, int numBCols, int numCRows, int numCCols)
 {
 	int col = blockIdx.x * (BLOCK_SIZE - WC + 1) + threadIdx.x;
 	int row = blockIdx.y * (BLOCK_SIZE - WC + 1) + threadIdx.y;
